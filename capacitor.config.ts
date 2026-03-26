fde31cfe8e191a6ae9a155040d15b89525726510
Ayo Mide <121489914+AyoNaim@users.noreply.github.com>
2026-03-26 09:29:38 +0000

    made changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,18 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.pancity.app',
-  appName: 'pancity',
-  webDir: 'public'
+  appId: "com.pancity.app",
+  appName: "pancity",
+  webDir: "out",
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: "#ffffffff",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+    },
+  },
 };
 
 export default config;
